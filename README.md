9b0ab004acbed4e6e97a2021ec9f7a6697f43bbb
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2023-12-04 19:43:29 +0200

    Stable Version Before adding the rest of functions

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hello
+_This is a markdown file_
+*README file*
+__Example__
+---
+<b style="color:red;"> Hello in Bold!</b>
---
+++ main.py
@@ -0,0 +1,22 @@
+###############################
+# Title: Library Program
+# Authors: Osama Ashraf & Muhammad Radwan
+# Version: v2.5
+# Description: -----
+################################
+
+from functions import *
+
+print('-----------------------------------------------')
+print('-----------------Library App-------------------')
+
+while True:
+    main_menu()
+    choice = input()
+    try:
+        check(choice)
+    except:
+        print('Enter a Valid value!')
+    
+    print('\n-----------------------------------------------')
+

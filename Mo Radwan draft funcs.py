864d074e70bd52017155224d9b9c32c426a7b694
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2023-12-23 21:27:08 +0200

    Some Edits

--- Mo Radwan draft funcs.py
+++ Mo Radwan draft funcs.py
@@ -15,61 +15,6 @@ def date_format           (instructions, Error_Massage):
             print(Error_Massage)
     return date
 
-def add                   (Library):
-    
-    book_name  = input("Book Name: ")
-    book_pages = integer_only("Number of pages: ", "Please enter the number of pages as an integer only")
-    date       = date_format("Date: ", "Please enter the date in DD/MM/YYYY format only")
-    status     = input("book status: ")
-    author     = input("Author of the book: ")
-   
-    book_details = {
-        "book name"  : book_name,
-        "book pages" : f"0 / {book_pages}",
-        "percentage" : "0%",
-        "date"       : date,
-        "status"     : status,
-        "rating"     : "not rated",
-        "author"     : author
-    }
-    Library.append(book_details)
-
-
-
-
-
-def remove                (library):
-    choose = choose_book(library)
-    library.remove(library[choose - 1])
- 
-
-def edit                  (library):
-    choose = choose_book(library)
-    print("What do you want to edit?")
-    print("1. Book name")
-    print("2. Book pages")
-    print("3. Date")
-    print("4. Status")
-    print("5. Author")
-    while True:
-            try:
-               edit = integer_only("Enter: ", "INVALID! Integers only")
-               if 0 < edit <= 5 :
-                   break
-            except:
-                print("OUT OF RANGE! Choose one of the above options") 
-    
-    if   edit == 1:
-        library[choose - 1]["book name"]  = input("Enter the updated book name: ")
-    elif edit == 2:
-        library[choose - 1]["book pages"] = input("Enter the updated book pages: ")
-    elif edit == 3:
-        library[choose - 1]["date"]       = input("Enter the updated date: ")
-    elif edit == 4:
-        library[choose - 1]["status"]     = input("Enter the updated status: ")
-    elif edit == 5:
-        library[choose - 1]["author"]     = input("Enter the updated author: ")
-
 def sort_book(library, key):    # Library    title
     keys_value = []
     for i in library:
@@ -82,10 +27,3 @@ def sort_book(library, key):    # Library    title
                 sorted_values.append(y)
     library.clear()
     library.append(sorted_values)
-
-# add   (MyLibrary)
-# print (MyLibrary)
-# read  (MyLibrary)
-# print (MyLibrary)
-# remove(MyLibrary)
-# print (MyLibrary)
